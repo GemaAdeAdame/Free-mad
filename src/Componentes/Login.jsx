0b580e819873b15e49266631e104da69d4fa2e16
GemaAdeAdame <gema.a.p.7@gmail.com>
2023-02-05 13:20:55 +0100

    commit

--- src/Componentes/Login.jsx
+++ src/Componentes/Login.jsx
@@ -5,8 +5,11 @@ import React, { component } from 'react';
 class Login extends React.Component{
   render() {
     return (
+      <div>
+
+    <p>Hola</p>
       <h1 class= "a-log">Logeatete</h1>
-     
+      </div>
     );
   }
 }
