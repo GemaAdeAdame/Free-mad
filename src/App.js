7c922591707bc6ba8e1f0950b5d07e275437b3b7
GemaAdeAdame <gema.a.p.7@gmail.com>
2023-02-07 20:45:58 +0100

    sbiendo formulario

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import "./App.css";
 import Home from "./Componentes/Home";
 import Login from "./Componentes/Login";
+import Loginvalidado from "./Componentes/Loginvalidado";
 import Frikicoders from "./Componentes/Frikicoders";
 import Eventosmad from "./Componentes/Eventosmad";
 import Eventosdrag from "./Componentes/Eventosdrag";
 import Carrito from "./Componentes/Carrito";
 
 
+
 function App() {
   return (
      <div>
       <Home />
-      <Login />
+     {/* <Login />*/}
+      <Loginvalidado />
       <Frikicoders />
       <Eventosmad />
       <Eventosdrag />
