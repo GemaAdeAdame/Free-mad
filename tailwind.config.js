79d8f1e18164681d13102c576933194d704f8c61
emmamme447 <emmarentero@gmail.com>
2023-02-08 22:04:52 +0100

    segundo commit emma

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/Componentes/Eventosmad.jsx",
+  ],
+  theme: {
+    extend: {
+    },
+  },
+  plugins: [],
+}
