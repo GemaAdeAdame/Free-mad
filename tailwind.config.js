1afb9c5c01a4dff6c0f9bb2ad61ee473c6594391
Nerea Pascual Picazo <npicazo22@gmail.com>
2023-02-09 14:55:40 +0100

    primer commit nerea

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/Componentes/Eventosdrag.jsx' , './src/Componentes/Eventosdrag.css'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
