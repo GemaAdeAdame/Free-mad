6a88952f4e825141e27f9f45229f60e7d717bea2
Ana Delgado del Castillo <116807861+anadelccc@users.noreply.github.com>
2023-02-10 15:50:33 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/Componentes/Eventosmad.jsx",
+  ],
+  theme: {
+    extend: {
+    },
+  },
+  plugins: [],
+}
+
