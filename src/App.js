7d3eca812011bed7576431f8ab89b2c9d4b565c7
Ana Delgado del Castillo <116807861+anadelccc@users.noreply.github.com>
2023-02-10 16:03:15 +0100

    commit completo

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import "./App.css";
 import Home from "./Componentes/Home";
 import Login from "./Componentes/Login";
+import Loginvalidado from "./Componentes/Loginvalidado";
 import Frikicoders from "./Componentes/Frikicoders";
 import Eventosmad from "./Componentes/Eventosmad";
 import Eventosdrag from "./Componentes/Eventosdrag";
 import Carrito from "./Componentes/Carrito";
+import ResponsiveAppBar from "./Componentes/Navbar";
+import BottomNavigation from "./Componentes/Footer";
+import Apiexterna from "./Componentes/Apiexterna";
+
 
 
 function App() {
   return (
      <div>
+      <ResponsiveAppBar />
       <Home />
-      <Login />
+      <Apiexterna />
+     {/* <Login />*/}
+      <Loginvalidado />
       <Frikicoders />
       <Eventosmad />
       <Eventosdrag />
       <Carrito />
+      <BottomNavigation />
      </div>
      
      
@@ -26,3 +35,5 @@ function App() {
 }
 
 export default App;
+
+
--- src/Componentes/Loginvalidado.jsx
+++ src/Componentes/Loginvalidado.jsx
@@ -6,10 +6,9 @@ import Box from "@mui/material/Box";
 import { TextField } from "@mui/material";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
-/*import Avatar from "@mui/material/Avatar";
-import { fontFamily } from "@mui/system";*/
-import Alert from '@mui/material/Alert';
-import Stack from '@mui/material/Stack';
+/*import { fontFamily } from "@mui/system";*/
+import swal from 'sweetalert';
+
 
 
 export const expresiones = {
@@ -78,7 +77,7 @@ const Loginvalidado = () => {
 
   const onSubmit = (data) => {
     console.log(data);
-    /*alert("Registrado/a correctamente✈️");*/
+    swal("Formulario enviado!","✈️" );
   };
   /*
   return (
@@ -221,6 +220,7 @@ export default Loginvalidado;*/
                       border: errors[field.name]
                         ? "3px solid red"
                         : "3px solid #FFD166",
+                        fontFamily: 'Mandali'
                     },
                     ".MuiInputBase-input": {
                       color: "#000",
@@ -234,6 +234,7 @@ export default Loginvalidado;*/
                       color: errors[field.name] ? "red" : "#000",
                       marginLeft: "10px",
                       background: value ? "aliceblue" : "transparent",
+                      fontFamily: 'Mandali'
                     },
                     ".MuiFormLabel-root.Mui-focused": {
                       color: "#000",
@@ -273,7 +274,7 @@ export default Loginvalidado;*/
               backgroundColor: "#645CBB",
               color: "white",
               borderRadius: "50px",
-              fontFamily: 'Poppins',
+              fontFamily: 'Mandali' ,
               fontSize: "15px",
               "&:hover": {
                 backgroundColor: "#06D6A0",
@@ -283,235 +284,9 @@ export default Loginvalidado;*/
           >
             Enviar
           </Button>
-          <Stack sx={{ width: '100%' }} spacing={30}>
-             <Alert variant="filled" severity="success">
-             ¡Usuario/a registrada!
-             </Alert>
-           </Stack>
         </form>
       </Box>
     </div>
   );
 };
 export default Loginvalidado;
-
-
-
-
-
-/*import React, { useEffect, useState } from "react";
-import { Controller, useForm } from "react-hook-form";
-import { pink } from "@mui/material/colors";
-import Checkbox from "@mui/material/Checkbox";
-import { TextField } from "@mui/material";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import background from "../images/fondoneon.jpg";
-
-
-const Login = () => {
-  const {
-    register,
-    handleSubmit,
-    control,
-    formState: { errors, dirtyFields },
-    watch,
-  } = useForm({
-    defaultValues:{
-      name: undefined,
-      email: undefined,
-      password: undefined,
-      confirmPassword: undefined,
-    }
-  });
-  const customSubmit = (data) => {
-    console.log(data);
-    alert("¡Enviado correctamente✈️!");
-  };
-
-  const [colorInput, setColorInput] = useState("#fff7f7");
-  useEffect(() => {
-    let words = watch("prueba");
-    if (words === "react") {
-      setColorInput("#614ad3");
-    }
-  });
-  
-  const password = watch("password");
-
-  const emailPattern = /^((?!\.)[\w-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/
-  return (
-    <>
-      <div style={{ backgroundImage: `url(${background})` }}></div>
-      <h2 class="g-titulo">Login</h2>
-      <form onSubmit={handleSubmit(customSubmit)} className="form-react" noValidate>
-        <div className="form-control">
-          <Controller
-            name="email"
-            control={control}
-            defaultValue=""
-            rules={{
-              required: true,
-              pattern: emailPattern,
-            }}
-            render={({ field: { onChange, value } }) => (
-              <TextField
-                sx={{ width: "100%" }}
-                onChange={onChange}
-                label={"Email"}
-                variant="outlined"
-                type={"email"}
-                value={value}
-                required
-                error={
-                  !!errors["email"] ||
-                  (dirtyFields["email"] && !emailPattern.exec(value))
-                }
-                helperText={
-                  (dirtyFields["email"] && !emailPattern.exec(value)) ||
-                  !!errors["email"] ? (
-                    <small className="fail">El campo no puede estar vacío</small>
-                  ) : (
-                    " "
-                  )
-                }
-              />
-            )}
-          />
-          <Controller
-            control={control}
-            name="name"
-            rules={{
-              required: true,
-              minLength: 3,
-              maxLength: 15,
-            }}
-            defaultValue=""
-            render={({ field: { onChange, value } }) => (
-              <TextField
-                label="Nombre"
-                onChange={onChange}
-                value={value}
-                type="text"
-                required
-              />
-            )}
-          />
-          {errors.name?.type === "required" && (
-            <small className="fail">El campo no puede estar vacío</small>
-          )}
-          {errors.name?.type === "maxLength" && (
-            <small className="fail">El máximo de caracteres es 15</small> 
-          )}
-           {errors.name?.type === "minLength" && (
-            <small className="fail">El mínimo de caracteres es 3</small>
-           )}
-
-
-           <Controller
-            name={"confirmPassword"}
-            control={control}
-            defaultValue=""
-            rules={{
-              required: true,
-              validate: (value) => value === password,
-            }}
-            render={({ field: { onChange, value } }) => (
-              <TextField
-                sx={{ width: "100%" }}
-                onChange={onChange}
-                label={"Confirm Password"}
-                variant="outlined"
-                type={"password"}
-                name={"confirmPassword"}
-                id={"confirmPassword"}
-                value={value}
-                required
-                error={
-                  !!errors.confirmPassword ||
-                  (dirtyFields.confirmPassword && value !== password)
-                }
-                helperText={
-                  (dirtyFields.confirmPassword && value !== password) ||
-                  !!errors.confirmPassword ? (
-                    <Typography variant="error">
-                      {"The answers don't match"}
-                    </Typography>
-                  ) : (
-                    " "
-                  )
-                }
-              />
-            )}
-          />
-        </div>
-        <div className="form-control">
-          <label>Años</label>
-          <input
-            type="number"
-            {...register("age", {
-              required: true,
-              min: 18,
-              max: 100,
-            })}
-          />
-          {errors.age?.type === "required" && (
-            <small className="fail">El campo no puede estar vacío</small>
-          )}
-          {errors.age?.type === "min" && (
-            <small className="fail">La edad mínima es de 18 años</small>
-          )}
-          {errors.age?.type === "max" && (
-            <small className="fail">La edad máxima es de 100 años</small>
-          )}
-        </div>
-        <div className="form-control">
-          <label>Ciudad</label>
-          <input
-            type="text"
-            {...register("country", {
-              required: {
-                value: true,
-                message: "Custom Message: Ingrese los datos",
-              },
-            })}
-          />
-          {errors.country && (
-            <small className="fail">{errors.country.message}</small>
-          )}
-        </div>
-        <div className="form-control">
-          <input
-            {...register("prueba")}
-            style={{ backgroundColor: colorInput }}
-          />
-        </div>
-        <Controller
-          label="label"
-          control={control}
-          defaultValue=""
-          name="checkbox"
-          render={({ field: { onChange, value } }) => (
-            <Checkbox
-              value={value}
-              label="checkbox"
-              onChange={onChange}
-              sx={{
-                color: pink[800],
-                "&.Mui-checked": {
-                  color: pink[600],
-                },
-              }}
-            />
-          )}
-        />
-
-        <Button sx={{ width: "100%" }} variant="contained" type="submit">
-            Sign Up
-        </Button>
-      </form>
-    </>
-  );
-};
-
-export default Login;*/
